86031ae863e89502a8bd60f3c373f8eeb02c7b9c
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2025-10-19 15:53:21 +0100

    second-commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const tempValue = document.querySelector(".temp-input");
+const convertBtn = document.querySelector(".convertBtn");
+const error = document.querySelector(".error");
+const fromScale = document.querySelector(".from-scale");
+const toScale = document.querySelector(".to-scale");
+
+let value = tempValue.value;
+
+let fromUnit = fromScale.value;
+let toUnit = toScale.value;
+
+console.log(fromUnit, toUnit);
+
+const getUnitSymbol = (unit) => {
+  switch (unit) {
+    case "celcuis":
+      return "°C";
+    case "fahrenheit":
+      return "°F";
+    case "kelvin":
+      return "K";
+  }
+};
+
+const symbol = getUnitSymbol(toUnit);
+console.log(symbol);
+
+const handleError = () => {
+  console.log("clicked");
+
+  console.log(value);
+
+  if (isNaN(value) || value === "") {
+    error.style.display = "block";
+  } else {
+    error.style.display = "none";
+  }
+};
+
+convertBtn.addEventListener("click", handleError);
